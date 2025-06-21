564e755d130fe330b3904a2ac4e4eb821a0a6128
B.K.S.S.S MALLIK <sssmallik143@gmail.com>
2025-06-21 11:47:29 +0530

    Updated vercel url

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? process.env.FRONTEND_URL 
-    : ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    : ['http://localhost:5173', 'http://127.0.0.1:5173','https://btlregistrationsystem.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
